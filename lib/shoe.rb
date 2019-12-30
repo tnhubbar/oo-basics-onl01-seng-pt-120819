60d7f384a813e99cb547a5e05a703a6d007633d7
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-30 01:06:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
 # Make your shoe class here!
+class Shoe 
+  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand 
+  
+  def initialization (brand)
+    @brand = brand 
+  end 
+  
+  
+  
+end 
